20cd78980ec16a26a1fd292fc2d6b455f9013b26
Asm Shuvo <shuvo.sust.cse@gmail.com>
2024-05-10 03:53:14 +0600

    basic setup done with home url

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ezfvwv5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+
+    const assignmentsCollection = client
+      .db("assignmentDB")
+      .collection("assignments");
+    app.get("/assignments", async (req, res) => {
+      const cursor = assignmentsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result); // after this the localhost:5000/spots will show the data
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("StudySync is running");
+});
+app.listen(port, () => {
+  console.log(`server is runnig on port ${port}`);
+});
